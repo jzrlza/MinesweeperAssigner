cfa14c2827dfbfa6eb2d899a486b5fddb838b67c
jzrlza <parinvut12345@gmail.com>
2019-05-27 15:20:44 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Vue.JS Minesweeper Test Bombs Assigner</h1>
+    <div id="nav">
+     <p> This work is purposed to analyse by vision upon difficult minesweeper board problems when not yet an expert player. Only works for square board.</p>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VueDemo.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="hello">
+    <img src='@/assets/logo-mine.png'>
+    <p v-for="j in b_height"><a v-for="i in b_width">
+      <input type="button" style="color:white; background-color: white;"
+      :id="'btn' + i + '_' + j" v-on:click="addOrRemoveBomb(i,j)" :disabled="board[j][i] == 9" :value="board[j][i]"></input></a>
+    </p>
+    <p>---</p>
+    
+  </div>
+</template>
+
+<script>
+//import router from '@/router'
+
+export default {
+
+  name: 'VueDemo',
+  data () {
+    return {
+      b_height: 30,
+      b_width: 16,
+      board: [],
+      flag: [],
+      reveal: [],
+      start_bombs: 0,
+      direction: [],
+      gameState: 0,
+      bombs: 0,
+      remaining: 0,
+      flags: 0,
+      flagState: 0,
+      styles: ['white','blue','green','red','navy','maroon','teal','black','grey'],
+    }
+  },
+  methods: {
+    getNumberColor(no,x,y) {
+      if (no === "b") {
+        document.getElementById("btn"+x+"_"+y).style = {
+        'color': 'black'
+        };
+        return 'black';
+      } else {
+        document.getElementById("btn"+x+"_"+y).style = {
+        'color': 'white'
+        };
+        return 'white';
+      }
+    },
+    addOrRemoveBomb(x,y) {
+      console.log(x);
+      console.log(y);
+      if (this.board[y][x] === "b") {
+        this.board[y][x] = 0;
+        document.getElementById("btn"+x+"_"+y).value = this.board[y][x].toString();
+        var myElement = document.querySelector("#btn"+x+"_"+y);
+        myElement.style.color = "white";
+        myElement.style.backgroundColor = "white";
+      } else {
+        this.board[y][x] = "b";
+        document.getElementById("btn"+x+"_"+y).value = this.board[y][x].toString();
+        var myElement = document.querySelector("#btn"+x+"_"+y);
+        myElement.style.color = "black";
+        myElement.style.backgroundColor = "red";
+      }
+
+      this.assignNumbers();
+
+      console.log(this.board);
+
+    },
+    assignBombs() {
+      for (var k = 0; k < this.start_bombs; k++) {
+        var tempX = Math.floor(Math.random() * this.b_width) + 1;
+        var tempY = Math.floor(Math.random() * this.b_height) + 1;
+        while ((this.board[tempY])[tempX] === "b" || (this.board[tempY][tempX])) {
+          tempX = Math.floor(Math.random() * this.b_width) + 1;
+          tempY = Math.floor(Math.random() * this.b_height) + 1;
+        }
+        this.board[tempY][tempX] = "b";
+        this.remaining--;
+      }
+      for (var i = 1; i < this.b_width+1; i++) {
+        for (var j = 1; j < this.b_height+1; j++) {
+          if (this.board[j][i] === "b") {
+            this.bombs++;
+          }
+        }
+      }
+      //setProperty("bombCounter", "text", "0"+this.bombs);
+    },
+    assignNumbers() {
+
+      for (var i = 1; i < this.b_width+1; i++) {
+        for (var j = 1; j < this.b_height+1; j++) {
+          if (this.board[j][i] !== "b") {
+              this.board[j][i] = 0;
+              var myElement = document.querySelector("#btn"+i+"_"+j);
+              document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+              //document.getElementById("btn"+i+"_"+j).style = this.style0;
+              if (j !== 1 && i !== 1) {
+                if (this.board[j-1][i-1] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (j !== 1) {
+                if (this.board[j-1][i] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (j !== 1 && i !== this.b_width) {
+                if (this.board[j-1][i-(-1)] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (i !== 1) {
+                if (this.board[j][i-1] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (i !== this.b_width) {
+                if (this.board[j][i-(-1)] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (j !== this.b_height && i !==1) {
+                if (this.board[j-(-1)][i-1] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (j !== this.b_height) {
+                if (this.board[j-(-1)][i] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+              if (j !== this.b_height && i !== this.b_width) {
+                if (this.board[j-(-1)][i-(-1)] === "b") {
+                  this.board[j][i]++;
+                  document.getElementById("btn"+i+"_"+j).value = this.board[j][i].toString();
+                }
+              }
+
+              myElement.style.color = this.styles[this.board[j][i]];
+          }
+        }
+      }
+
+      var l = 1;
+      var m = 1;
+      for (l = 1; l < this.b_height+1; l++) {
+          for (m = 1; m < this.b_width+1; m++) {
+            if (this.board[l][m] !== 0 && this.board[l][m] !== "b") {
+              (this.reveal[l])[m] = this.board[l][m];
+            }
+          }
+      }
+
+    }
+  },
+  mounted(){
+
+      var y = 0;
+      var x = 0;
+
+      this.board = [ ];
+      for(y = 0; y < this.b_height+2; ++y) {
+          this.board[y] = [ ];
+          for(x = 0; x < this.b_width+2; ++x) {
+              if (y == 0 || y == this.b_height+1 || x == 0 || x == this.b_width+1){
+                this.board[y][x] = 9;
+              } else {
+                this.board[y][x] = 0;
+              }
+          }
+      }
+
+      this.flag = [ ];
+      for(y = 0; y < this.b_height+2; ++y) {
+          this.flag[y] = [ ];
+          for(x = 0; x < this.b_width+2; ++x) {
+              this.flag[y][x] = 0;
+          }
+      }
+
+      this.reveal = [ ];
+      for(y = 0; y < this.b_height+2; ++y) {
+          this.reveal[y] = [ ];
+          for(x = 0; x < this.b_width+2; ++x) {
+              if (y == 0 || y == this.b_height+1 || x == 0 || x == this.b_width+1){
+                this.reveal[y][x] = 9;
+              } else {
+                this.reveal[y][x] = 0;
+              }
+          }
+      }
+
+      this.remaining = this.b_height*this.b_width;
+
+      this.assignBombs();
+      this.assignNumbers();
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  width: 250px;
+}
+
+</style>
